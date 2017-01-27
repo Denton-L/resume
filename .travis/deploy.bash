155ae9598b993318191b88f72701573876467a34
Denton Liu <liu.denton@gmail.com>
2017-01-27 00:21:36 -0800

    Update deploy script

--- .travis/deploy.bash
+++ .travis/deploy.bash
@@ -7,17 +7,20 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-git config user.name "${GIT_NAME}"
-git config user.email "${GIT_EMAIL}"
-git remote add deploy "${GIT_URL}"
-git checkout --orphan gh-pages
+DEPLOY_BRANCH=gh-pages
+DEPLOY_FILES='*.pdf'
+
+git config user.name "$GIT_NAME"
+git config user.email "$GIT_EMAIL"
+git remote add deploy "$GIT_URL"
+git checkout --orphan $DEPLOY_BRANCH
 git rm --cached -r .
-git add -f *.pdf
-git commit -m "Automatic build by Travis CI: ${TRAVIS_COMMIT}"
+git add -f $DEPLOY_FILES
+git commit -m "Automatic build by Travis CI: $TRAVIS_COMMIT"
 
-openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ${KEY_LOCATION}.enc -out ${KEY_LOCATION} -d
-chmod 600 ${KEY_LOCATION}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $KEY_LOCATION.enc -out $KEY_LOCATION -d
+chmod 600 $KEY_LOCATION
 eval $(ssh-agent)
-ssh-add ${KEY_LOCATION}
+ssh-add $KEY_LOCATION
 
-git push --force deploy gh-pages
+git push --force deploy $DEPLOY_BRANCH
